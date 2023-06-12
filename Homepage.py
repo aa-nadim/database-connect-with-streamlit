fe753a54f1aec235308620720ece5fff31f878cb
aa-nadim <nadim.ice.nstu@gmail.com>
2023-06-12 18:25:03 +0600

    modify

--- Homepage.py
+++ Homepage.py
@@ -41,12 +41,3 @@ if authentication_status:
     st.title("Main Page")
 
     st.sidebar.success("Select a page above.")
-
-    # if "my_input" not in st.session_state:
-    #     st.session_state["my_input"] = ""
-
-    # my_input = st.text_input("Input a text here", st.session_state["my_input"])
-    # submit = st.button("Submit")
-    # if submit:
-    #     st.session_state["my_input"] = my_input
-    #     st.write("You have enteted: ", my_input)
--- pages/2_Projects.py
+++ pages/2_Projects.py
@@ -1,5 +1,3 @@
 import streamlit as st
 
 st.title("Projects")
-
-# st.write("You have entered", st.session_state["my_input"])
